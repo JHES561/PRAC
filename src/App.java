5c92add7be91e5a8246bb2ed62c5b7803998a6c3
JHES561 <thejhes2006@gmail.com>
2025-03-14 11:27:37 -0500

    aprendiendo

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner plus = new Scanner(System.in);
+        System.out.println("Hello, World!");
+        //aprendiendo 
+        System.out.println("ingrese numero");
+        int numero = plus.nextInt();
+
+        System.out.println("tu numero es "+ numero);
+        plus.close();
+    }
+}
